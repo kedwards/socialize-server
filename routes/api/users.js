982375cc97a0f8cb19d0dc277f6f2ef69df7cb1c
Kevin Edwards <kedwards@kevinedwards.ca>
2019-09-27 20:21:18 -0600

    added user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
 
-// @route    POST api/users
-// @desc     Register user
-// @access   Public
+// @route   POST api/users
+// @desc    Register user
+// @access  Public
 router.post(
     "/",
     [
@@ -25,7 +27,40 @@ router.post(
             return res.status(400).json({ errors: errors.array() });
         }
 
-        res.send("User Route");
+        const { name, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            if (user) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: "User already exists" }] });
+            }
+
+            const avatar = gravatar.url(email, {
+                s: "200",
+                r: "pg",
+                d: "mm",
+            });
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password,
+            });
+
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+            res.send("user registered");
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server error");
+        }
     },
 );
 
