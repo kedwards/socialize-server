6305d7f1e34c208a0f48b6dd38b39c90e95a0a43
Kevin Edwards <kedwards@kevinedwards.ca>
2019-10-11 11:55:22 -0600

    ready for production

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const formData = require("express-form-data");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -13,9 +14,9 @@ app.use(cors());
 app.use(express.json({ extended: false }));
 app.use(formData.parse());
 
-app.get("/", (req, res) => {
-    res.send(`Socialize API v1`);
-});
+// app.get("/", (req, res) => {
+//     res.send(`Socialize API v1`);
+// });
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
@@ -23,6 +24,22 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/uploads", require("./routes/api/uploads"));
+
+// Serve static assets in prod
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("../socialize-client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(
+                __dirname,
+                "../socialize-client",
+                "build",
+                "indexedDB.html",
+            ),
+        );
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
